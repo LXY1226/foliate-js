ba7b3c5dc4142150c5c59b7b3b6934b8f27d0a59
LXY <767763591@qq.com>
2024-09-15 15:31:36 +0800

    Make cbz full screen-style

--- comic-book.js
+++ comic-book.js
@@ -33,7 +33,7 @@ export const makeComicBook = ({ entries, loadBlob, getSize }, file) => {
         size: getSize(name),
     }))
     book.toc = files.map(name => ({ label: name, href: name }))
-    book.rendition = { layout: 'pre-paginated' }
+    book.rendition = { layout: 'pre-paginated', spread: 'none' }
     book.resolveHref = href => ({ index: book.sections.findIndex(s => s.id === href) })
     book.splitTOCHref = href => [href, null]
     book.getTOCFragment = doc => doc.documentElement
